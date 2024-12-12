e1f8e956ae4773d690ae2af6c94ad33937bae2f8
shahinexy <shahinexy@gmail.com>
2024-12-12 12:08:34 +0600

    Prettier setup completed

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 // const express = require('express')
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-const app: Application = express()
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
 // const port = 3000
 
 //parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
-  res.send(a)
-})
+
+  res.send(a);
+});
 
 export default app;
