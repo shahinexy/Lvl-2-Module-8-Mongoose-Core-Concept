02fb129e57a77745e645646d4604c8e532b4286f
shahinexy <shahinexy@gmail.com>
2025-01-03 22:30:09 +0600

    handle cast error of mongoose

--- src/app/error/handleValidationError.ts
+++ src/app/error/handleValidationError.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
-import { TErrorSource } from '../interface/error';
+import { TErrorSource, TGenericErrorResponse } from '../interface/error';
 
-const handleValidationError = (err: mongoose.Error.ValidationError) => {
+const handleValidationError = (
+  err: mongoose.Error.ValidationError,
+): TGenericErrorResponse => {
   const errorSources: TErrorSource = Object.values(err.errors).map(
     (value: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
       return {
--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -6,9 +6,9 @@ import { TErrorSource } from '../interface/error';
 import config from '../config';
 import handleZodError from '../error/handleZodError';
 import handleValidationError from '../error/handleValidationError';
+import handleCastError from '../error/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next): any => {
-  
   //setting default value
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Something went wrong';
@@ -16,29 +16,36 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next): any => {
   let errorSources: TErrorSource = [
     {
       path: '',
-      message: 'Something went wrong'
-    }
-  ]
+      message: 'Something went wrong',
+    },
+  ];
 
-  if(err instanceof ZodError){
-    const simplifiedError = handleZodError(err)
-    
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources
-  }else if(err?.name === 'ValidationError'){
-    const simplifiedError = handleValidationError(err)
-    
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSources = simplifiedError?.errorSources
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
 
   return res.status(statusCode).json({
     success: false,
     message,
     errorSources,
-    stack: config.NODE_ENV === 'development'? err?.stack : null
+    err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
