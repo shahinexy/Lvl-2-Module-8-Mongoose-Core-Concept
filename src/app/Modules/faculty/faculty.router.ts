0f61c54cd9a3e94580ef606a0e9479f6563a784d
shahinexy <shahinexy@gmail.com>
2025-01-13 15:31:47 +0600

    work on auth authorization

--- src/app/Modules/faculty/faculty.router.ts
+++ src/app/Modules/faculty/faculty.router.ts
@@ -4,12 +4,13 @@ import { FacultyControllers } from './faculty.controller'
 import valideteRequest from '../../middlwares/validetRequest'
 import { facultyValidations } from './faculty.validation'
 import auth from '../../middlwares/auth'
+import { USER_ROLE } from '../user/user.constant'
 
 const router = express.Router()
 
 // it will call controller
 
-router.get('/', auth(), FacultyControllers.getAllFacultys)
+router.get('/', auth(USER_ROLE.admin, USER_ROLE.faculty), FacultyControllers.getAllFacultys)
 
 router.get('/:id', FacultyControllers.getFaculty)
 
--- src/app/middlwares/auth.ts
+++ src/app/middlwares/auth.ts
@@ -4,6 +4,7 @@ import AppError from '../error/AppError';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../Modules/user/user.interface';
+import { UserModel } from '../Modules/user/user.model';
 
 const auth = (...requiredRole: TUserRole[]) => {
   return catchAsync(
@@ -21,6 +22,29 @@ const auth = (...requiredRole: TUserRole[]) => {
       ) as JwtPayload;
 
       const { role, userId, iat } = decoded;
+
+      // check if user exists
+      const isUserExists = await UserModel.findOne({ id: userId }).select(
+        '+password',
+      );
+
+      if (!isUserExists) {
+        throw new AppError(404, 'This user is not found');
+      }
+
+      // check if user is already delete
+      const isDelete = isUserExists.isDeleted;
+
+      if (isDelete) {
+        throw new AppError(403, 'This user is deleted');
+      }
+
+      // check if the use is blocked
+      const userStatus = isUserExists.status;
+
+      if (userStatus === 'blocked') {
+        throw new AppError(403, 'This user is Bolocked');
+      }
 
       if (requiredRole && !requiredRole.includes(role)) {
         throw new AppError(401, 'You are not authorize');
