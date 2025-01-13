3435820cea313522186dfd21979a99da35298563
shahinexy <shahinexy@gmail.com>
2025-01-13 19:06:37 +0600

    refreshtoken added on cookie

--- src/app/Modules/auth/auth.controller.ts
+++ src/app/Modules/auth/auth.controller.ts
@@ -1,33 +1,43 @@
-import catchAsync from "../../utils/catchAsync";
-import sendResponse from "../../utils/sendResponse";
-import { AuthServices } from "./auth.service";
+import config from '../../config';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AuthServices } from './auth.service';
 
+const loginUser = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body);
 
-const loginUser = catchAsync(async(req,res)=>{
-    const result = await AuthServices.loginUser(req.body);
+  const { refreshToken, accessToken, needPasswordChange } = result;
 
-    sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: 'Auth login successfully',
-        data: result,
-      });
-})
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
 
-const changePassword = catchAsync(async(req,res)=>{
-    const {...passwordData} = req.body;
-    
-    const result = await AuthServices.changePassword(req.user, passwordData);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Auth login successfully',
+    data: {
+      accessToken,
+      needPasswordChange,
+    },
+  });
+});
 
-    sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: 'Password Changed successfully',
-        data: result,
-      });
-})
+const changePassword = catchAsync(async (req, res) => {
+  const { ...passwordData } = req.body;
+
+  const result = await AuthServices.changePassword(req.user, passwordData);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Password Changed successfully',
+    data: result,
+  });
+});
 
 export const AuthControllers = {
-    loginUser,
-    changePassword
-}
+  loginUser,
+  changePassword,
+};
--- src/app/Modules/auth/auth.service.ts
+++ src/app/Modules/auth/auth.service.ts
@@ -4,10 +4,13 @@ import { UserModel } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 import bcrypt from 'bcrypt';
 import jwt, { JwtPayload } from 'jsonwebtoken';
+import { CreateToken } from './auth.utils';
 
 const loginUser = async (payload: TLoginUser) => {
   // check if user exists
-  const isUserExists = await UserModel.findOne({ id: payload.id }).select('+password');
+  const isUserExists = await UserModel.findOne({ id: payload.id }).select(
+    '+password',
+  );
 
   if (!isUserExists) {
     throw new AppError(404, 'This user is not found');
@@ -40,65 +43,91 @@ const loginUser = async (payload: TLoginUser) => {
   // create token and send to the client
   const jwtPayload = {
     userId: isUserExists.id,
-    role: isUserExists.role
-  }
+    role: isUserExists.role,
+  };
 
-  const accessToken = jwt.sign(
+  // const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+  //   expiresIn: '10d',
+  // });
+
+  const accessToken = CreateToken(
     jwtPayload,
     config.jwt_access_secret as string,
-    { expiresIn: '10d' },
+    config.jwt_access_expires_in as string,
+  );
+
+  const refreshToken = CreateToken(
+    jwtPayload,
+    config.jwt_refresh_secret as string,
+    config.jwt_refresh_expires_in as string,
   );
 
   return {
     accessToken,
-    needPasswordChange: isUserExists.needsPasswordChange
+    refreshToken,
+    needPasswordChange: isUserExists.needsPasswordChange,
   };
 };
 
-const changePassword = async (userData: JwtPayload, payload: {oldPassword: string, newPassword: string})=>{
- // check if user exists
- const isUserExists = await UserModel.findOne({ id: userData.userId }).select('+password');
+const changePassword = async (
+  userData: JwtPayload,
+  payload: { oldPassword: string; newPassword: string },
+) => {
+  // check if user exists
+  const isUserExists = await UserModel.findOne({ id: userData.userId }).select(
+    '+password',
+  );
 
- if (!isUserExists) {
-   throw new AppError(404, 'This user is not found');
- }
+  if (!isUserExists) {
+    throw new AppError(404, 'This user is not found');
+  }
 
- // check if user is already delete
- const isDelete = isUserExists.isDeleted;
+  // check if user is already delete
+  const isDelete = isUserExists.isDeleted;
 
- if (isDelete) {
-   throw new AppError(403, 'This user is deleted');
- }
+  if (isDelete) {
+    throw new AppError(403, 'This user is deleted');
+  }
 
- // check if the use is blocked
- const userStatus = isUserExists.status;
+  // check if the use is blocked
+  const userStatus = isUserExists.status;
 
- if (userStatus === 'blocked') {
-   throw new AppError(403, 'This user is Bolocked');
- }
+  if (userStatus === 'blocked') {
+    throw new AppError(403, 'This user is Bolocked');
+  }
 
- // check if the password correct
- const isPasswordMatched = await bcrypt.compare(
-   payload.oldPassword,
-   isUserExists.password,
- );
+  // check if the password correct
+  const isPasswordMatched = await bcrypt.compare(
+    payload.oldPassword,
+    isUserExists.password,
+  );
 
- if (!isPasswordMatched) {
-   throw new AppError(400, 'Password do not matched');
- }
+  if (!isPasswordMatched) {
+    throw new AppError(400, 'Password do not matched');
+  }
 
- // has new password 
- const newHashedPassword = await bcrypt.hash(payload.newPassword, Number(config.bcrypt_salt_round));
+  // has new password
+  const newHashedPassword = await bcrypt.hash(
+    payload.newPassword,
+    Number(config.bcrypt_salt_round),
+  );
 
-  await UserModel.findOneAndUpdate({
-    id: userData.userId,
-    role: userData.role
-  }, {password: newHashedPassword, needsPasswordChange: false, passwordChangedAt: new Date})
+  await UserModel.findOneAndUpdate(
+    {
+      id: userData.userId,
+      role: userData.role,
+    },
+    {
+      password: newHashedPassword,
+      needsPasswordChange: false,
+      passwordChangedAt: new Date(),
+    },
+  );
 
   return null;
-} 
+};
 
 export const AuthServices = {
   loginUser,
-  changePassword
+  changePassword,
 };
